201f8f5a2116d40095e8fa39845a89e751f7de9b
root <root@139-162-62-177.ip.linodeusercontent.com>
2022-09-17 12:34:11 +0000

    part 1 comitted

--- memory_check.sh
+++ memory_check.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-free
+usage() { echo "Usage: $0 [-c critical threshold(percentage)] [-w warning threshold(percentage)] [-e email]" 1>&2; exit 1; }
+
+while getopts ":c:w:e:" o; do
+    case "${o}" in
+        c)
+            c=${OPTARG}
+            ;;
+        w)
+            w=${OPTARG}
+            ;;
+        e)
+            e=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [ -z "${c}" ] || [ -z "${w}" ] || [ -z "${e}" ]; then
+    usage
+elif ! [[ "$c" =~ ^[0-9]+$ ]]; then
+    usage
+elif ! [[ "$w" =~ ^[0-9]+$ ]]; then
+    usage
+elif [ $c -lt $w ]; then
+    usage
+fi
