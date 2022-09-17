721414930fc5d6c3cddf6016da05895512b636fe
root <root@139-162-62-177.ip.linodeusercontent.com>
2022-09-17 12:49:56 +0000

    part 2 comitted

--- memory_check.sh
+++ memory_check.sh
@@ -27,3 +27,19 @@ elif ! [[ "$w" =~ ^[0-9]+$ ]]; then
 elif [ $c -lt $w ]; then
     usage
 fi
+
+USED_MEMORY=$( free | grep Mem: | awk '{ print $3 }' )
+TOTAL_MEMORY=$( free | grep Mem: | awk '{ print $2 }' )
+a=`expr $USED_MEMORY \* $c`
+CRITICAL_THRESHOLD=`expr $a / 100`
+b=`expr $USED_MEMORY \* $w`
+WARNING_THRESHOLD=`expr $b / 100`
+
+
+if [ $USED_MEMORY -ge $CRITICAL_THRESHOLD ]; then
+    echo "2"; exit
+elif [ $USED_MEMORY -ge $WARNING_THRESHOLD ] && [ $USED_MEMORY -lt $CRITICAL_THRESHOLD ]; then
+    echo "1"; exit
+elif [ $USED_MEMORY -lt $WARNING_THRESHOLD ]; then
+    echo "0"; exit
+fi
